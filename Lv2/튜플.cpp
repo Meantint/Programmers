4e84e206590a995969b8b4a17802167c94d29e3c
Meantint <50372451+Meantint@users.noreply.github.com>
2020-06-09 20:57:43 +0900

    튜플

---
+++ Lv2/튜플.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#define pii pair<int, int>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer(0);
+
+    vector<pii> vec(100001);    // Pair Vector 배열 초기화
+    for (int i = 0; i < 100001; ++i) {
+        vec[i].second = i;
+    }
+
+    for (int i = 2; i < s.size() - 2; ++i) {
+        int num = 0;
+        if (s[i] < '0' || s[i] > '9') continue;
+        while (s[i] >= '0' && s[i] <= '9') { // s[i]가 숫자라면 몇 인지 다 읽는 while문
+            num *= 10;
+            num += (s[i] - '0');
+            i++;
+        }
+        vec[num].first++;
+    }
+    sort(vec.begin(), vec.end(), greater<pii>());
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i].first == 0) break;
+        answer.push_back(vec[i].second);
+    }
+
+    return answer;
+}
+
+// Test!!
+int main()
+{
+    // vector<int> vec = solution("{{123}}");
+    vector<int> vec = solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
+    for (int i = 0; i < vec.size() - 1; ++i) {
+        cout << vec[i] << ", ";
+    }
+    cout << vec[vec.size() - 1] << endl;
+
+    return 0;
+}
