ef1187bca23bbfd71ffaeeed709dfd6c77cf3e91
Meantint <mistinca@naver.com>
2020-06-18 01:18:53 +0900

    Update 크레인 인형뽑기 게임

--- Lv1/크레인 인형뽑기 게임.cpp
+++ Lv1/크레인 인형뽑기 게임.cpp
@@ -3,27 +3,28 @@
 
 using namespace std;
 
-int solution(vector<vector<int>> board, vector<int> moves) {
-        int answer = 0;
+int solution(vector<vector<int>> board, vector<int> moves)
+{
+    int answer = 0;
 
-        vector<int> box;
-        for (int i = 0; i < moves.size(); ++i) {
-                for (int j = 0; j < board.size(); ++j) {
-                        if (board[j][moves[i] - 1] == 0) continue;
+    vector<int> box;
+    for (int i = 0; i < moves.size(); ++i) {
+        for (int j = 0; j < board.size(); ++j) {
+            if (board[j][moves[i] - 1] == 0)
+                continue;
 
-                        if (box.size() != 0 && board[j][moves[i] - 1] == box.back()) {
-                                ++answer;
-                                box.pop_back();
-                                board[j][moves[i] - 1] = 0;
-                                break;
-                        }
-                        else {
-                                box.push_back(board[j][moves[i] - 1]);
-                                board[j][moves[i] - 1] = 0;
-                                break;
-                        }
-                }
+            if (box.size() != 0 && board[j][moves[i] - 1] == box.back()) {
+                ++answer;
+                box.pop_back();
+                board[j][moves[i] - 1] = 0;
+                break;
+            } else {
+                box.push_back(board[j][moves[i] - 1]);
+                board[j][moves[i] - 1] = 0;
+                break;
+            }
         }
+    }
 
-        return answer * 2;
+    return answer * 2;
 }
