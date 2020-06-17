32d592a1febb2d7580708171e13a856ce50a8b34
Meantint <mistinca@naver.com>
2020-06-18 01:19:09 +0900

    Update 크레인 인형뽑기 게임 new_version

--- Lv1/크레인 인형뽑기 게임 new_version.cpp
+++ Lv1/크레인 인형뽑기 게임 new_version.cpp
@@ -6,24 +6,20 @@ using namespace std;
 vector<int> basket(0);
 int answer = 0;
 
-void change(vector<vector<int>> &board, int idx)
+void change(vector<vector<int>>& board, int idx)
 {
     int rowSize = board.size();
-    for (int i = 0; i < rowSize; ++i)
-    {
+    for (int i = 0; i < rowSize; ++i) {
         // 배열의 원소 값이 0일 때
         if (board[i][idx - 1] == 0)
             continue;
         // 바구니의 마지막 인형과 방금 넣은 인형이 같을 때
-        if (!basket.empty() && board[i][idx - 1] == basket.back())
-        {
+        if (!basket.empty() && board[i][idx - 1] == basket.back()) {
             basket.pop_back();
             board[i][idx - 1] = 0;
             answer += 2;
             break;
-        }
-        else
-        {
+        } else {
             basket.push_back(board[i][idx - 1]);
             board[i][idx - 1] = 0;
             break;
@@ -34,8 +30,7 @@ void change(vector<vector<int>> &board, int idx)
 int solution(vector<vector<int>> board, vector<int> moves)
 {
     int Size = moves.size();
-    for (int i = 0; i < Size; ++i)
-    {
+    for (int i = 0; i < Size; ++i) {
         change(board, moves[i]);
     }
 
